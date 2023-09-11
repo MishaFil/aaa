5b2b472989665cd5e2edda6f0b302f17a82a8a5b
MishaFil <fme300605@yandex.ru>
2023-09-11 22:38:19 +0700

    123

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%3l#h!x1w3m8mzw%vfrc=^(!n7*+v&z&t*g^k6h1ioq0g!9g(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
