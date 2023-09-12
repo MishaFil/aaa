c8c0bd3b4391a43e450ce11e227494e46d11f454
MishaFil <fme300605@yandex.ru>
2023-09-12 19:00:14 +0700

    Сделал фильтр

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,31 @@
 """blog.admin."""
+from adminfilters.autocomplete import AutoCompleteFilter
+from adminfilters.combo import ChoicesFieldComboFilter
+from adminfilters.dj import DjangoLookupFilter
+from adminfilters.json import JsonFieldFilter
+from adminfilters.numbers import NumberFilter
+from adminfilters.querystring import QueryStringFilter
+from adminfilters.value import ValueFilter
 from django.contrib import admin
-from .models import Post, Category, Comments
+from django.contrib.admin import ModelAdmin
+from django.forms import JSONField
 
+from . import models
+from .models import Post,  Comments
+class CommentInline(admin.TabularInline):
+    model = Comments
+    extra = 0
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     """."""
     list_display = ("title", "author", "category")
     fields = ("title", "author", "text", "category", "published_date")
+    inlines = [CommentInline]
 
 
-class CommentInline(admin.TabularInline):
-    model = Comments
-    extra = 0
 
 
-inlines = [CommentInline]
+class FilterDepotManager:
+    pass
+
