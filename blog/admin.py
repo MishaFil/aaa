c8bb9442674a2cd260c173a8eb2160d565b2c9d6
MishaFil <fme300605@yandex.ru>
2023-09-12 11:15:14 +0700

    не работают категории

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 """blog.admin."""
 from django.contrib import admin
-from .models import Post
+from .models import Post,Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     """."""
-    list_display = ("title", "author")
-    fields = ("title", "author", "text")
+    list_display = ("title", "author","category")
+    fields = ("title", "author", "text","category")
 
-    # inlines = []
+
--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+class Category(models.Model):
+    name = models.CharField(max_length=64, verbose_name='название')
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +18,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
